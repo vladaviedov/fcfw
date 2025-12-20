fa88c7f97027c4d1a03f8d3a206d5133b3850ea3
Vladyslav Aviedov <vladaviedov@protonmail.com>
2025-12-19 21:53:09 -0500

    blink test

--- src/main.c
+++ src/main.c
@@ -3,12 +3,18 @@
 #include <avr/io.h>
 #include <util/delay.h>
 
+#include "pins.h"
 #include "io.h"
 
 int main(void) {
 	io_init();
 
 	while (1) {
+		io_pin_write(&pin_io_acq_led, L_HIGH);
+		io_pin_write(&pin_io_hold_led, L_HIGH);
+		_delay_ms(500);
+		io_pin_write(&pin_io_acq_led, L_LOW);
+		io_pin_write(&pin_io_hold_led, L_LOW);
 		_delay_ms(500);
 	}
 }
