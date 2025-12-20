a6325428c66edc5dbb12eb76a94b6072421ee6d9
Vladyslav Aviedov <vladaviedov@protonmail.com>
2025-12-19 21:35:59 -0500

    create pin map

---
+++ src/pins.c
@@ -0,0 +1,24 @@
+#include "pins.h"
+
+#include <avr/io.h>
+
+const pin pin_io_hold_btn = { .port = MP_A, .bit = 0 };
+const pin pin_io_div_a = { .port = MP_A, .bit = 1 };
+const pin pin_io_div_b = { .port = MP_A, .bit = 2 };
+const pin pin_io_hold_led = { .port = MP_A, .bit = 3 };
+const pin pin_io_acq_led = { .port = MP_B, .bit = 3 };
+
+const pin pin_s_data = { .port = MP_A, .bit = 4 };
+const pin pin_s_clk = { .port = MP_A, .bit = 5 };
+const pin pin_s_load = { .port = MP_A, .bit = 6 };
+
+const pin pin_ctr_reset = { .port = MP_A, .bit = 7 };
+
+const pin pin_ref_reset = { .port = MP_B, .bit = 0 };
+const pin pin_ref_div0 = { .port = MP_C, .bit = 3 };
+const pin pin_ref_div1 = { .port = MP_C, .bit = 4 };
+const pin pin_ref_div2 = { .port = MP_C, .bit = 5 };
+const pin pin_ref_trig = { .port = MP_C, .bit = 0 };
+
+const pin pin_disp_sda = { .port = MP_B, .bit = 1 };
+const pin pin_disp_scl = { .port = MP_C, .bit = 1 };
---
+++ src/pins.h
@@ -0,0 +1,40 @@
+#pragma once
+
+#include <avr/io.h>
+
+typedef enum {
+	MP_A,
+	MP_B,
+	MP_C,
+} mcu_port;
+
+typedef struct {
+	mcu_port port;
+	uint8_t bit;
+} pin;
+
+// IO
+extern const pin pin_io_hold_btn;
+extern const pin pin_io_div_a;
+extern const pin pin_io_div_b;
+extern const pin pin_io_hold_led;
+extern const pin pin_io_acq_led;
+
+// Shift register
+extern const pin pin_s_data;
+extern const pin pin_s_clk;
+extern const pin pin_s_load;
+
+// Counter
+extern const pin pin_ctr_reset;
+
+// Time reference
+extern const pin pin_ref_reset;
+extern const pin pin_ref_div0;
+extern const pin pin_ref_div1;
+extern const pin pin_ref_div2;
+extern const pin pin_ref_trig;
+
+// Display
+extern const pin pin_disp_sda;
+extern const pin pin_disp_scl;
